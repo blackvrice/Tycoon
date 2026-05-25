191f93836ddc51b9ee39695f974f9195ed1d2309
blackvrice <45651641+blackvrice@users.noreply.github.com>
2026-05-25 12:06:14 +0900

    Build world tilemap from enum layout

---
+++ Assets/Scripts/Tilemap/GameWorldGrid.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:74f63732c7847211b5e66e62945b6b089b626a6437a49357bbecd3d95506921d
+size 8227
---
+++ Assets/Tests/PlayMode/GameWorldGridPlayModeTests.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:560a60b65d579cee8b6a16b4f8a9ef7b849110dbbe7ad87006a0582da6e7cefe
+size 5730
