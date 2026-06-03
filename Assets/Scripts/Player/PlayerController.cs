006162987b17249a05977d9227a040604e26ae1a
blackvrice <45651641+blackvrice@users.noreply.github.com>
2026-06-03 10:41:42 +0900

    Prioritize harvest on item use

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e34cbb923219919a97a654cae28d9c6e1fb9faac9a2ae80f9098c4605603e19b
-size 14891
+oid sha256:4d9776a7cb120296b2af652de300527218b7ea763ba23b717498127bae1d3c63
+size 15390
