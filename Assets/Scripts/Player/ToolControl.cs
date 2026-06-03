e75d854adde013135308e830f0e1e953139416bd
blackvrice <45651641+blackvrice@users.noreply.github.com>
2026-06-03 10:10:22 +0900

    Add tool animation fallback events

--- Assets/Scripts/Player/ToolControl.cs
+++ Assets/Scripts/Player/ToolControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eabe698ab87308c51e3140d0376461e9555505fa62d99b9c29979737286fc1a8
-size 4802
+oid sha256:75c173d0b13ecffd734d8eb42869984b9f818e2e3d11947ed3c354c91c4a697e
+size 7204
