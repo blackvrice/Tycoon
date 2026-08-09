6a3a74a2be919e52951d3bfc1b7f43cc4325808d
blackvrice <45651641+blackvrice@users.noreply.github.com>
2026-08-09 15:38:17 +0900

    Add smooth scene fade transitions

---
+++ Assets/Scripts/Core/SceneTransitionController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:00c08377d89257570e6ab47dea8cc415c66ba40c31de732afa9aa614be0c0df1
+size 8390
---
+++ Assets/Tests/PlayMode/SceneTransitionPlayModeTests.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:219a3903b915816a82a69c9ab257abd2f6c64c26e2d794d2126786d583d2ad81
+size 4249
